d9aeb556abc5c1e840beef2157bb8522803c41d0
MYuPopov <beemax@bk.ru>
2021-12-21 13:31:39 +0300

    Подключил Проект 2. Первые const

--- src/modules/one.js
+++ src/modules/one.js
@@ -1,5 +1,11 @@
 const oneJS = () => {
-	console.log("one.js is loaded");
+	const timerHours = document.getElementById("timer-hours");
+	const timerMinutes = document.getElementById("timer-minutes");
+	const timerSeconds = document.getElementById("timer-seconds");
+
+	console.log(timerHours, timerMinutes, timerSeconds);
+
+	console.log("one.js is loaded 1");
 };
 
 export default oneJS;
--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
 	devServer: {
 		hot: true,
 		static: {
-			// directory: "./dist",
+			directory: "./dist",
 			watch: true,
 		},
 	},
