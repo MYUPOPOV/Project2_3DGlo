b25514eb2425fd41cd1b661e67a07c99d1e58b6a
MYuPopov <beemax@bk.ru>
2021-12-26 16:04:55 +0300

    Обязательное задание Урока 21

--- src/modules/textValidation.js
+++ src/modules/textValidation.js
@@ -1,29 +1,55 @@
 const textValidation = () => {
 	/* Главная страница */
 	const form1Name = document.getElementById('form1-name');
-	console.log('~ form1Name', form1Name);
 	const form1Email = document.getElementById('form1-email');
-	console.log('~ form1Email', form1Email);
 	const form1Phone = document.getElementById('form1-phone');
-	console.log('~ form1Phone', form1Phone);
 
 	/* Модальное окно */
 	const form3Name = document.getElementById('form3-name');
-	console.log('~ form3Name', form3Name);
 	const form3Phone = document.getElementById('form3-phone');
-	console.log('~ form3Phone', form3Phone);
 	const form3Email = document.getElementById('form3-email');
-	console.log('~ form3Email', form3Email);
 
 	/* Остались вопросы? */
 	const form2Name = document.getElementById('form2-name');
-	console.log('~ form2Name', form2Name);
 	const form2Email = document.getElementById('form2-email');
-	console.log('~ form2Email', form2Email);
 	const form2Phone = document.getElementById('form2-phone');
-	console.log('~ form2Phone', form2Phone);
 	const form2Message = document.getElementById('form2-message');
-	console.log('~ form2Message', form2Message);
+
+	/* Поля ввода type=text и placeholder="Ваше сообщение"  */
+	form1Name.addEventListener('input', (e) => {
+		e.target.value = e.target.value.replace(/[^а-яА-Я- ]/, '');
+	});
+	form2Name.addEventListener('input', (e) => {
+		e.target.value = e.target.value.replace(/[^а-яА-Я- ]/, '');
+	});
+	form3Name.addEventListener('input', (e) => {
+		e.target.value = e.target.value.replace(/[^а-яА-Я- ]/, '');
+	});
+	form2Message.addEventListener('input', (e) => {
+		e.target.value = e.target.value.replace(/[^а-яА-Я- ]/, '');
+	});
+
+	/* Поля ввода type=email  */
+	form1Email.addEventListener('input', (e) => {
+		e.target.value = e.target.value.replace(/[^a-zA-Z@-_.!~*']/, '');
+	});
+	form2Email.addEventListener('input', (e) => {
+		e.target.value = e.target.value.replace(/[^a-zA-Z@-_.!~*']/, '');
+	});
+	form3Email.addEventListener('input', (e) => {
+		e.target.value = e.target.value.replace(/[^a-zA-Z@-_.!~*']/, '');
+	});
+
+	/*  Поля ввода type=tel  */
+	form1Phone.addEventListener('input', (e) => {
+		e.target.value = e.target.value.replace(/[^0-9()-]/, '');
+	});
+	form2Phone.addEventListener('input', (e) => {
+		e.target.value = e.target.value.replace(/[^0-9()-]/, '');
+	});
+	form3Phone.addEventListener('input', (e) => {
+		e.target.value = e.target.value.replace(/[^0-9()-]/, '');
+	});
 };
 
 export default textValidation;
