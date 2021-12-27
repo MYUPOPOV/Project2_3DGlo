c35cb4888a9ffb763e7131e16449204754f45621
MYuPopov <beemax@bk.ru>
2021-12-27 10:47:59 +0300

    Усложненное задание 21

--- src/modules/textValidation.js
+++ src/modules/textValidation.js
@@ -1,6 +1,4 @@
 const textValidation = () => {
-	console.log('textValidation.js is loaded');
-
 	/* Главная страница */
 	const form1Name = document.getElementById('form1-name');
 	const form1Email = document.getElementById('form1-email');
@@ -17,73 +15,49 @@ const textValidation = () => {
 	const form2Phone = document.getElementById('form2-phone');
 	const form2Message = document.getElementById('form2-message');
 
-	const textFormFormat = function (form) {
-		form.value = form.value.replace(/[^а-яА-Я- ]/, '');
+	/* Функция форматирования Полей ввода type=text и placeholder="Ваше сообщение"  */
+	const textFormFormat = function () {
+		this.addEventListener('input', (e) => {
+			e.target.value = e.target.value.replace(/[^а-яА-Я- ]/, '');
+		});
+		this.addEventListener('blur', (e) => {
+			e.target.value = e.target.value.replace(/[^а-яА-Я- ]+/, '');
+			e.target.value = e.target.value.replace(/\s+/, ' ');
+			e.target.value = e.target.value.replace(/^[-\s]+/, '');
+			e.target.value = e.target.value.replace(/[-\s]+$/, '');
+			e.target.value = e.target.value[0].toUpperCase() + e.target.value.substr(1).toLowerCase();
+		});
+	};
+	/* Функция форматирования Полей ввода type=email  */
+	const emailFormFormat = function () {
+		this.addEventListener('input', (e) => {
+			e.target.value = e.target.value.replace(/[^a-zA-Z0-9@\-_.!~\*']/, '');
+		});
+		this.addEventListener('blur', (e) => {
+			e.target.value = e.target.value.replace(/[^a-zA-Z0-9@\-_.!~\*']+/, '');
+			e.target.value = e.target.value.replace(/\s+/, ' ');
+			e.target.value = e.target.value.replace(/^[-\s]+/, '');
+			e.target.value = e.target.value.replace(/[-\s]+$/, '');
+		});
+	};
+	/* Функция форматирования Полей ввода type=tel  */
+	const telFormFormat = function () {
+		this.addEventListener('input', (e) => {
+			e.target.value = e.target.value.replace(/[^0-9()-]/, '');
+		});
+		this.addEventListener('blur', (e) => {
+			e.target.value = e.target.value.replace(/[^0-9()-]+/, '');
+			e.target.value = e.target.value.replace(/\s+/, ' ');
+			e.target.value = e.target.value.replace(/^[-\s]+/, '');
+			e.target.value = e.target.value.replace(/[-\s]+$/, '');
+		});
 	};
 
-	/* Поля ввода type=text и placeholder="Ваше сообщение"  */
-	form1Name.addEventListener('input', (e) => {
-		e.target.value = e.target.value.replace(/[^а-яА-Я- ]/, '');
-	});
+	[form1Name, form2Name, form3Name, form2Message].forEach((item) => textFormFormat.bind(item)());
+	[form1Email, form2Email, form3Email].forEach((item) => emailFormFormat.bind(item)());
+	[form1Phone, form2Phone, form3Phone].forEach((item) => telFormFormat.bind(item)());
 
-	form2Name.addEventListener('input', (e) => {
-		e.target.value = e.target.value.replace(/[^а-яА-Я- ]/, '');
-	});
-	form3Name.addEventListener('input', (e) => {
-		e.target.value = e.target.value.replace(/[^а-яА-Я- ]/, '');
-	});
-	form2Message.addEventListener('input', (e) => {
-		e.target.value = e.target.value.replace(/[^а-яА-Я- ]/, '');
-	});
-
-	/* Поля ввода type=email  */
-	form1Email.addEventListener('input', (e) => {
-		e.target.value = e.target.value.replace(/[^a-zA-Z0-9@\-_.!~\*']/, '');
-	});
-	form2Email.addEventListener('input', (e) => {
-		e.target.value = e.target.value.replace(/[^a-zA-Z0-9@\-_.!~\*']/, '');
-	});
-	form3Email.addEventListener('input', (e) => {
-		e.target.value = e.target.value.replace(/[^a-zA-Z0-9@\-_.!~\*']/, '');
-	});
-
-	/*  Поля ввода type=tel  */
-	form1Phone.addEventListener('input', (e) => {
-		e.target.value = e.target.value.replace(/[^0-9()-]/, '');
-	});
-	form2Phone.addEventListener('input', (e) => {
-		e.target.value = e.target.value.replace(/[^0-9()-]/, '');
-	});
-	form3Phone.addEventListener('input', (e) => {
-		e.target.value = e.target.value.replace(/[^0-9()-]/, '');
-	});
-
-	/* Усложненное задание */
-	/* 1. Должны удаляться все символы, кроме допустимых. */
-	/*   В поля ввода type=text и placeholder="Ваше сообщение" позволить ввод только кириллицы в любом регистре, дефиса и пробела.
-  В поля ввода type=email позволить ввод только латиницы в любом регистре, цифры и спецсимволы:  @  -  _  . ! ~ * '
-  (Собака , Дефис , Подчеркивание , Точка , Восклицательный знак , Тильда , Звездочка , Одинарная кавычка)   
-  В поля ввода type=tel позволить ввод только цифр, круглых скобок и дефис */
-	form1Name.addEventListener('blur', (e) => {
-		e.target.value = e.target.value.replace(/[^а-яА-Я- ]+/, '');
-	});
-
-	/* 2. Несколько идущих подряд пробелов или дефисов должны заменяться на один. */
-	form1Name.addEventListener('blur', (e) => {
-		e.target.value = e.target.value.replace(/\s+/, ' ');
-	});
-
-	/* 3. Пробелы и дефисы в начале и конце значения должны удаляться. */
-	form1Name.addEventListener('blur', (e) => {
-		e.target.value = e.target.value.replace(/^[-\s]+/, '');
-		e.target.value = e.target.value.replace(/[-\s]+$/, '');
-	});
-
-	/* 4. Для поля type=text Первая буква каждого слова должна приводиться к верхнему регистру, а все остальные — к нижнему. */
-	form1Name.addEventListener('blur', (e) => {
-		const str = e.target.value;
-		e.target.value = str[0].toUpperCase() + str.substr(1).toLowerCase();
-	});
+	console.log('textValidation.js is loaded');
 };
 
 export default textValidation;
